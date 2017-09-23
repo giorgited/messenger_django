564309ac3b150f5f0b5868bb076fa2110fdf1d98
gio <gxt150530@utdallas.edu>
2017-09-23 01:25:39 -0500

    added registration/login/logout

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,22 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def home(request):
     return render(request, 'accounts/home.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        print(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/accounts/')
+        else:
+            return render(request, 'accounts/register.html', {'form': UserCreationForm()})
+    else:
+        form = UserCreationForm()
+        args = {
+            'form': form
+        }
+        return render(request, 'accounts/register.html', args)
