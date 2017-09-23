cb202dcc0541b582e8d354f93d71a2d2028bd0df
gio <gxt150530@utdallas.edu>
2017-09-23 02:35:58 -0500

    added user account information, and user account edit

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     url(r'^$', views.home),
     url(r'^login/$', login, {'template_name': 'accounts/login.html'}),
     url(r'^logout/$', logout, {'template_name': 'accounts/logout.html'}),
-    url(r'^register/$', views.register, name='register'),  
+    url(r'^register/$', views.register, name='register'),      
+    url(r'^user_info/$', views.user_info, name='user_info'),  
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,22 +1,39 @@
 from django.shortcuts import render, HttpResponse, redirect
 from django.contrib.auth.forms import UserCreationForm
-
+from accounts.forms import RegistrationForm
+from accounts.forms import EditUserForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserChangeForm
 # Create your views here.
 def home(request):
-    return render(request, 'accounts/home.html')
+    return render(request, 'accounts/home.html', {'user': request.user})
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = RegistrationForm(request.POST)
         print(request.POST)
         if form.is_valid():
             form.save()
             return redirect('/accounts/')
         else:
-            return render(request, 'accounts/register.html', {'form': UserCreationForm()})
+            return render(request, 'accounts/register.html', {'form': RegistrationForm()})
     else:
-        form = UserCreationForm()
+        form = RegistrationForm()
         args = {
             'form': form
         }
         return render(request, 'accounts/register.html', args)
+
+@login_required
+def user_info(request):
+    if request.method == "POST":
+        form = EditUserForm(request.POST, instance=request.user)
+
+        if form.is_valid():
+            form.save()
+
+        form = EditUserForm(instance = request.user)
+        return render(request, 'accounts/userInfo.html', {'user': request.user, 'form': form})
+    else:
+        form = EditUserForm(instance = request.user)
+        return render(request, 'accounts/userInfo.html', {'user': request.user, 'form': form})
